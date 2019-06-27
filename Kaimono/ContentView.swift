a87ec5383ed160eaf8865acc593bb0f852d86008
John Forde <fordee@me.com>
2019-06-28 08:04:34 +1200

    Initial Commit

---
+++ Kaimono/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Kaimono
+//
+//  Created by John Forde on 28/06/19.
+//  Copyright © 2019 John Forde. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
